58976622acff80adea1c5a3486c9f433f8f963d8
王通明 <wangtongming@dianxiaomi.com>
2022-09-29 11:35:40 +0800

    更新

---
+++ README.md
@@ -0,0 +1,57 @@
+## mm-cli
+
+集成项目开发模板 mm-cli
+
+### 安装
+
+```
+  npm i mm-cli -g
+```
+
+### 所有命令
+
+```
+  帮助:
+      mm --help
+
+  查看版本:
+      mm -V 或 mm --version
+
+  查看模板列表:
+      mm list 或 mm ls
+
+  查看是否最新版本:
+      mm upgrad 或 mm u
+
+  创建项目:
+      mm create <project>
+
+  添加模板:
+      mm add 或 mm a
+
+  删除模板:
+      mm delete 或 mm del
+
+```
+
+### 创建项目
+
+```
+  mm create <project>
+```
+
+### 项目依赖安装
+
+```
+  pnpm i
+  or
+  npm i
+```
+
+### 项目启动
+
+```
+  pnpm run dev
+  or
+  npm run dev
+```
---
+++ bin/cli.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+import { program } from "commander";
+import { readFile } from "fs/promises";
+
+import myList from "../lib/core/list.js";
+import myCreate from "../lib/core/create.js";
+import myAdd from "../lib/core/add.js";
+import myDelete from "../lib/core/delete.js";
+import myUpgrade from "../lib/core/upgrade.js";
+
+// 获取版本号
+program.version(
+  JSON.parse(await readFile(new URL("../package.json", import.meta.url)))
+    .version,
+  "-V, --version"
+);
+
+myCreate(program);
+
+myList(program);
+
+myAdd(program);
+
+myDelete(program);
+
+myUpgrade(program);
+
+program.parse(process.argv);
+
+if (!program.args.length) {
+  program.help();
+}
